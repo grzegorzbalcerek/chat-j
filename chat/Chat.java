98ddae7382b6f154f9e3336cc976dce950518006
Grzegorz Balcerek <gb@grzegorzbalcerek.net>
2016-10-08 23:16:48 +0200

    04-map

--- chat/Chat.java
+++ chat/Chat.java
@@ -1,5 +1,8 @@
 package chat;
 
+import javaslang.collection.HashMap;
+import javaslang.collection.Map;
+import javaslang.collection.Seq;
 import nz.sodium.Cell;
 import nz.sodium.Stream;
 import nz.sodium.StreamSink;
@@ -10,11 +13,21 @@ public class Chat {
     public final Stream<Message> output;
 
     Chat() {
+        Cell<Map<String, Session>> sessions = joins
+                .accum(HashMap.empty(), (session, map) -> map.put(session.nick, session));
         Cell<Stream<Message>> mergedMessages =
-                joins.accum(new Stream<>(), (session, merged) -> session.messages.orElse(merged));
+                sessions.map(m -> mergeStreams(m.values().map(session -> session.messages)));
         Stream<Message> messages = Cell.switchS(mergedMessages);
         this.output = messages
                 .orElse(joins.map(session -> new Message(session.nick, "joins")));
+    }
+
+    public static <T> Stream<T> mergeStreams(Seq<Stream<T>> streams) {
+        Stream<T> acc = new Stream<>();
+        for (Stream<T> stream : streams) {
+            acc = acc.orElse(stream);
+        }
+        return acc;
     }
 
 }
